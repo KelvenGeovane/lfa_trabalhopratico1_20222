83bd6d5e32b17d05aebe8661a3529906a21f5984
Ana Beatriz Carvalho <ana.bcarvalho@souunit.com.br>
2022-09-25 16:34:04 -0300

    Separação dos estados

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-dado = open("automato.txt", "r")
-listaAutomato = []
-automatos = dado.readlines()
+def add_lista(list_, nome):
+    list_return = []
+    for i in list_:
+        if i.strip("\n") == nome:
+            break
+        list_return.append(i.strip("\n"))
+    return list_return
 
-for automato in automatos:
-    listaAutomato.append(automato.split('\n')[0])
-    dado.close()
 
-print(listaAutomato)
+def lerarquivo():
+
+    dado = open("automato.txt", "r")
+    automatos = dado.readlines()
+
+    for i in range(len(automatos)):
+
+        if automatos[i] == "#states\n":
+            states = add_lista(automatos[1+1:], "#initial")
+
+        elif automatos[i] == "#initial\n":
+            initial = add_lista(automatos[i+1:], "#accepting")
+
+        elif automatos[i] == "#accepting\n":
+            accepting = add_lista(automatos[i+1:], "#alphabet")
+
+        elif automatos[i] == "#alphabet\n":
+            alphabet = add_lista(automatos[i+1:], "#transitions")
+
+        elif automatos[i] == "#transitions\n":
+
+            transitions = add_lista(automatos[i+1:], "")
+        dado.close()
+    return states, initial, accepting, alphabet, transitions
+
+
+states, initial, accepting, alphabet, transitions = lerarquivo()
+print(states)
